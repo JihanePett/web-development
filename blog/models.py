685d509fe129e35ca283d1b6fc9bd27cb60ecd54
JihanePett <jihane.petticrew@gmail.com>
2021-02-20 15:18:35 +0000

    styling

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from profiles.models import UserProfile
-
 from django.urls import reverse
 
 # Create your models here.
--- webdevelopment/settings.py
+++ webdevelopment/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'blog',
-    'profiles',
     'crispy_forms',
 ]
 
