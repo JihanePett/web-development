b9b3e955e6c8aec0d12f8a79cdad09ef69584b4f
JihanePett <jihane.petticrew@gmail.com>
2021-02-23 15:14:19 +0000

    updated the region and aws bucket name

--- webdevelopment/settings.py
+++ webdevelopment/settings.py
@@ -169,9 +169,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 if 'USE_AWS' in os.environ:
-    # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'ckz8780-boutique-ado'
-    AWS_S3_REGION_NAME = 'us-east-1'
+        # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'webdevjp'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
