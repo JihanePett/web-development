7c837eeb21f9b408e12c6b66adbefbe37cf4100b
JihanePett <jihane.petticrew@gmail.com>
2021-02-25 09:59:37 +0000

    deployment 2

--- webdevelopment/settings.py
+++ webdevelopment/settings.py
@@ -115,6 +115,7 @@ WSGI_APPLICATION = 'webdevelopment.wsgi.application'
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 # }
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
